890501ac56d09612fe0072a5d89ca53827c076f2
K <63994608+celeron34@users.noreply.github.com>
2025-06-15 10:36:31 +0900

    最後のパーティが選ばれるバグを修正

--- main.py
+++ main.py
@@ -796,9 +796,9 @@ class ApproveView(discord.ui.View):
                 thread = message.channel
                 joinMember = party.joins[message]
                 print(f'JoinMember: {joinMember}')
-                for party in ROBIN_GUILD.parties:
-                    if isinstance(party, LightParty) and party.isMember(joinMember):
-                        party.removeMember(joinMember)
+                for p in ROBIN_GUILD.parties:
+                    if isinstance(p, LightParty) and p.isMember(joinMember):
+                        p.removeMember(joinMember)
                 for p in {p for p in ROBIN_GUILD.parties if joinMember in p.joins.values()}: # 参加リアクション全削除
                     await p.message.remove_reaction(ROBIN_GUILD.RECLUTING_EMOJI, joinMember)
                 await party.joinMember(Participant(joinMember, set(role for role in joinMember.roles if role in ROBIN_GUILD.ROLES.keys())))
